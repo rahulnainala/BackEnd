b0f0a69fd70d485ed04b1231dfc7c225228fd930
Rahul Nainala <rahulnainala@Rahuls-MacBook-Pro.local>
2024-11-12 22:28:24 +0530

    NPM packages learned

--- NodeJs/NotesApp/index.js
+++ NodeJs/NotesApp/index.js
@@ -1,3 +1,14 @@
 const fs = require("fs");
 
 fs.appendFileSync("notes.txt", "I am from Hyderabad");
+
+var validator = require("validator");
+
+let y = "https://rahulnainala.com";
+
+let x = validator.isURL(y);
+
+if (x === true) {
+  console.log(`Hello ${x}`);
+}
+console.log(x);
