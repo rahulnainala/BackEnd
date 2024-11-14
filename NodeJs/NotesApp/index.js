00398f2745847c721058a753d90e858d28443205
Rahul Nainala <rahulnainala2@gmail.com>
2024-11-14 19:25:14 +0530

    Argv Learnings

--- NodeJs/NotesApp/index.js
+++ NodeJs/NotesApp/index.js
@@ -1,10 +1,53 @@
-console.log(process.argv);
-const command = process.argv[2];
+const yargs = require("yargs");
+//add,remove,read,list
 
-if (command === "Add") {
-  console.log("user added");
-} else if (command === "Delete") {
-  console.log("user Deleted");
-} else {
-  console.log("Default");
-}
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note Title",
+      demandOption: true,
+      type: "string",
+    },
+    body: {
+      describe: "Body of Note",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Title : " + argv.title);
+    console.log("Body : " + argv.body);
+  },
+});
+
+//creates a remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a Note",
+  handler: function () {
+    console.log("Remove a Note !");
+  },
+});
+
+//creates a read command
+yargs.command({
+  command: "read",
+  describe: "Read a Note",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+
+//create a list command
+yargs.command({
+  command: "list",
+  describe: "List all Notes",
+  handler: function () {
+    console.log("Listing all Notes");
+  },
+});
+
+yargs.parse();
