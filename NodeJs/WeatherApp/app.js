fb35c34efab830dcffea0090176baf60ca76cc87
Rahul Nainala <rahulnainala2@gmail.com>
2024-11-27 22:40:47 +0530

    Async Sample Code

---
+++ NodeJs/WeatherApp/app.js
@@ -0,0 +1,11 @@
+console.log("Starting Code .....");
+
+setTimeout(() => {
+  console.log("Set Timeout has ended after 4 seconds");
+}, 4000);
+
+setTimeout(() => {
+  console.log("Set Timeout has ended after 2 seconds");
+}, 2000);
+
+console.log("Stopping Code .....");
